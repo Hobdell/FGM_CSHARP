00d7dcd448277ad9048ac68d6b20fefe92ac6800
Richard Kavanagh <richard.kavanagh@hscic.gov.uk>
2015-09-17 22:06:44 +0100

    Initial Code

--- Example/Program.cs
+++ Example/Program.cs
@@ -10,6 +10,46 @@ namespace Example
     {
         static void Main(string[] args)
         {
+            Parameters resourceParameters = CreateResources.CreateQueryResource();
+            Organization resourceOrganization = CreateResources.CreateOrganizationResource();
+            Practitioner resourcePractitioner = CreateResources.CreatePractitionerResource();
+            MessageHeader resourceMessageHeader = CreateResources.CreateMessageHeaderResource();
+
+            string x1 = FhirSerializer.SerializeResourceToXml(resourceParameters);
+            string x2 = FhirSerializer.SerializeResourceToXml(resourceOrganization);
+            string x3 = FhirSerializer.SerializeResourceToXml(resourcePractitioner);
+            string x4 = FhirSerializer.SerializeResourceToXml(resourceMessageHeader);
+
+
+            Bundle msg = new Bundle();
+            msg.Id = "13daadee-26e1-4d6a-9e6a-7f4af9b58877";
+            Meta metadata = new Meta();
+            metadata.Profile = new string[] { "urn:fhir.nhs.uk:profile/NHS-FGM-Bundle-QueryParameters" };
+            msg.Meta = metadata;
+
+            msg.Type = Bundle.BundleType.Message;
+
+            msg.Entry.Add(new Bundle.BundleEntryComponent()
+            {
+                Resource = resourceMessageHeader
+            });
+
+            msg.Entry.Add(new Bundle.BundleEntryComponent()
+            {
+                Resource = resourceParameters
+            });
+
+            msg.Entry.Add(new Bundle.BundleEntryComponent()
+            {
+                Resource = resourcePractitioner
+            });
+
+            msg.Entry.Add(new Bundle.BundleEntryComponent()
+            {
+                Resource = resourceOrganization
+            });
+
+            string x5 = FhirSerializer.SerializeResourceToXml(msg);
         }
     }
 }
